3de0b28eb419535ae5f5c766c1c0681c8784ba08
灿烂 <1481086138@qq.com>
2024-08-18 23:17:15 +0800

    模板语法

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties = {
+  window: {
+    location: {
+      href: 'https://www.google.com'
+    },
+    message: '全局message变量',
+    Test: {
+      name: '全局name变量'
+    }
+  }
+}
+app.mount('#app')
+
