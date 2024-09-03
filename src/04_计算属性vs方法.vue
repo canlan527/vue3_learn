50ff10c9d254bebc6cccbe31278a85900e8f0783
灿烂 <1481086138@qq.com>
2024-09-03 23:56:18 +0800

    计算属性vs方法

---
+++ src/04_计算属性vs方法.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>
+      <button @click="a++">A++</button>
+      <button @click="b++">B++</button>
+    </div>
+
+    <p> 
+      <span>computedA: {{ computedA }}</span> 
+      <span>computedB: {{ computedB }}</span>
+    </p>
+    
+    <p>
+      <span>methedA: {{ incrementA() }}</span>
+      <span>methedB: {{ incrementB() }}</span>
+    </p>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+const a = ref(1)
+const b = ref(100)
+
+// computed
+const computedA = computed(() => {
+  console.log('computedA 执行了')
+  return a.value + 1;
+})
+
+const computedB = computed(() => {
+  console.log('computedB 执行了')
+  return b.value + 1;
+})  
+
+// methods
+const incrementA = () => {
+  console.log('incrementA 执行了')
+  return a.value + 1;
+}
+const incrementB = () => {
+  console.log('incrementB 执行了')
+  return b.value + 1;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
