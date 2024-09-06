8b11dcf8b48df71296769c2ced6ae24e3bfa3a15
灿烂 <1481086138@qq.com>
2024-09-07 00:12:51 +0800

    class案例

---
+++ src/05_class.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div :class="demoClass">
+      <h1 :class="[titleClass, sliderClass]">灿烂</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, computed } from 'vue'
+
+const isSliderShow = ref(false)
+
+const demoClass = reactive({
+  'green': true,
+  'active': false,
+  'position-relative': true
+})
+
+// computed
+const sliderClass = computed(() => {
+  return {
+    'slider': isSliderShow.value,
+  }
+})
+
+const titleClass = reactive({
+  'position-absolute': true,
+})
+
+setTimeout(() => {
+  isSliderShow.value = true
+}, 2000)
+
+</script>
+
+<style lang="scss" scoped>
+.position-relative {
+  position: relative;
+  width: 300px;
+  height: 100px;
+}
+
+.position-absolute {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.slider {
+  right: 0;
+  transform: translateX(260px);
+  transition: all 1s ease-in-out;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,10 @@
 <template>
   <div>
-    <!-- <h1 :class="[isActive, error]">Hello Vue 3</h1> -->
-    <!-- 三元运算符 -->
-    <!-- <h1 :class="[isActive? 'active': '', error]">hello vue3</h1> -->
-    <!-- <h1 :style="{ color: isActive? 'green' : 'black' }">Hello Vue 3</h1> -->
-    <!-- <h1 :style="[{color: isActive? 'green' : 'black'}, {fontSize: '20px'}]">Hello Vue 3</h1> -->
-    <h1 :style="[styleObj1, styleObj2]">Hello Vue 3</h1>
+    <h1>Hello Vue 3</h1>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
-
-// const isActive = ref(false)
-// const isActive = ref('active')
-// const error = ref('text-danger')
-
-
-const styleObj1 = reactive({
-  color: 'green',
-  fontSize: '20px'
-})
-
-const styleObj2 = reactive({
-  backgroundColor: 'blue',
-  border: '1px solid red'
-})
-
-setTimeout(() => {
-  // isActive.value = true
-  styleObj1.color = 'black'
-}, 2000)
 
 </script>
 
