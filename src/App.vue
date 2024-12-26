6d99c835e2cfcb97d0781e7363092ff81e430603
灿烂 <1481086138@qq.com>
2024-12-26 20:06:09 +0800

    element plus

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,59 @@
 <template>
   <div>
-    <div class="container">
-      <h1>任务管理器</h1>
-      <div class="task-stats">
-        <p>任务总数：{{ taskCount }}</p>
-        <p>已完成数：{{ completedTaskCount }}</p>
-      </div>
-      <input type="text" v-model="newTaskTitle" placeholder="添加新任务" @keyup.enter="addTask" />
-      <TaskList :tasks="pendingTasks"  title="进行中任务"/>
-      <TaskList :tasks="completedTasks" title="已完成任务" />
+    <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 
-      <!-- loading -->
-       <div class="loading" v-if="loading">
-        <div class="spinner"></div>
-       </div>
-    </div>
+  <div class="mb-4">
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </div>
+
+  <div class="mb-4">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </div>
+
+  <div>
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
+  </div>
+  <el-icon :size="size" :color="color">
+      <Edit />
+    </el-icon>
+
+    <Edit />
   </div>
 </template>
 
 <script setup>
-import TaskList from './components/Task/TaskList.vue';
-import { ref, computed, onMounted} from 'vue'
-import useTaskStore from '@/store/useTaskStore'
-// store
-const taskStore = useTaskStore()
-// store数据
-const tasks = computed(() => taskStore.tasks)
-const loading = computed(() => taskStore.loading)
-const completedTasks = computed(() => taskStore.completedTasks)
-const pendingTasks = computed(() => taskStore.pendingTasks)
-const taskCount = computed(() => taskStore.taskCount)
-const completedTaskCount = computed(() => taskStore.completedTaskCount)
-
-// 组件数据
-const newTaskTitle = ref('')
-
-onMounted(async () => {
-  await taskStore.fetchTask()
-})
-
-async function addTask() {
-  if(newTaskTitle.value.trim()) {
-    await taskStore.addTask({
-      title: newTaskTitle.value,
-      completed: false,
-    })
-    newTaskTitle.value = ''
-  }
-}
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
 </script>
 
-<style scoped>
-.container {
-  width: 600px;
-  margin: 50px auto;
-  padding: 20px;
-  background: #f9f9f9;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
+<style lang="scss" scoped>
 
-.task-stats {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-}
-
-input {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-h1 {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-.loading {
-  text-align: center;
-  color: #999;
-  font-size: 1.2em;
-}
-
-.spinner {
-  border: 4px solid rgba(0, 0, 0, 0.1);
-  border-left-color: #22a6b3;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  animation: spin 1s linear infinite;
-  margin: 20px auto;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 导入路由
 import router from '@/router'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 const app = createApp(App)
 const pinia = createPinia()
+
+// 注册所有图标
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//   app.component(key, component)
+// }
 
 app.config.globalProperties = {
   window: {
@@ -22,6 +30,7 @@ app.config.globalProperties = {
 
 app.use(pinia)
 app.use(router)
+// app.use(ElementPlus)
 
 app.mount('#app')
 
