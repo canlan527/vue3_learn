21c8238668bc73fe2d21b3eb14db331d2e8be619
灿烂 <1481086138@qq.com>
2025-02-08 23:50:34 +0800

    使用proxy验证函数参数

--- src/deepVue/04_观察者模式2.js
+++ src/deepVue/04_观察者模式2.js
@@ -32,3 +32,25 @@ function observer(target) {
 
   return proxy
 }
+
+
+// proxy 验证参数
+function sum (a, b) {
+  return a+b;
+}
+function validate(target, ...types) {
+  const proxy = new Proxy(target, {
+    apply(target, thisBinding, args) {
+      types.forEach((type, index) => {
+        if(typeof args[index] !== type ) {
+          throw new Error(`第${index+1}参数 ${args[index]} 的类型应该为${type}`)
+        }
+      })
+      return Reflect.apply(target, thisBinding, args)
+    }
+  })
+  return proxy
+}
+
+const sumProxy = validate(sum, 'number', 'number')
+console.log(sumProxy(1, '12'))
